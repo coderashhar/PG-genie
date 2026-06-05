82ae42a7362ba82708c2b2e3f815c1672bbf30aa
Mohd. Ashhar Khan <ashhar2003.khan@gmail.com>
2026-06-05 22:54:10 +0530

    chore: cleanup test files

--- test-api.js
+++
@@ -1,3 +0,0 @@
-const { PUT } = require('./.next/server/app/api/users/profile/route.js');
-
-// I can't easily mock next-auth in the compiled output.
--- test-valid.ts
+++
@@ -1,4 +0,0 @@
-import { validateProfileUpdate } from './src/lib/validation';
-
-const result = validateProfileUpdate({ batch: '2026' }, 'student');
-console.log(result);
